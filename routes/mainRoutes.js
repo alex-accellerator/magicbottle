7ae8209a679b537d9936bfa8fd143fe2670c6d8d
Phil Wee <cyber_peewee@yahoo.com>
2015-03-07 22:40:15 -0800

    fix routing

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -6,3 +6,19 @@ Router.route('/', {
     SEO.set({ title: 'Home - ' + Meteor.App.NAME });
   }
 });
+
+Router.route('/validation', {
+  name: 'validation',
+  action: function () {
+    this.render('validation');
+    SEO.set({ title: 'Patient Validation - ' + Meteor.App.NAME });
+  }
+});
+
+Router.route('/feedback', {
+  name: 'feedback',
+  action: function () {
+    this.render('feedback');
+    SEO.set({ title: 'Patient Feedback - ' + Meteor.App.NAME });
+  }
+});
